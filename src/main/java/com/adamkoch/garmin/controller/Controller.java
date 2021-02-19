5adacb302e285a1ba80eb9a987504c8e57d56440
Adam Koch <adam@dogtired.org>
2021-02-18 19:51:23 -0600

    error handling; set return type to application/json

--- src/main/java/com/adamkoch/garmin/controller/Controller.java
+++ src/main/java/com/adamkoch/garmin/controller/Controller.java
@@ -1,6 +1,6 @@
 package com.adamkoch.garmin.controller;
 
-import static org.apache.logging.log4j.LogManager.*;
+import static org.apache.logging.log4j.LogManager.getLogger;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -14,13 +14,16 @@ import com.adamkoch.garmin.services.TokenService;
 import com.adamkoch.garmin.services.UserService;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -44,50 +47,45 @@ public class Controller {
 
   private Pattern deviceStatePattern = Pattern.compile("[a-zA-Z_]{1,20}");
 
-//  GET https://api.qa.fitpay.ninja/users/:userId
-//  GET https://api.qa.fitpay.ninja/users/:userId/devices
-//  GET https://api.qa.fitpay.ninja/users/:userId/creditCards
-
   public Controller() {
     LOGGER.info("Starting controller");
   }
 
-  @GetMapping("/compositeUsers/{userId}")
+  @GetMapping(path = "/compositeUsers/{userId}", produces = "application/json")
   public User composite(
       @PathVariable String userId,
       @RequestParam(value = "creditCardState", required = false) String creditCardState,
-      @RequestParam(value = "deviceState", required = false) String deviceState,
-      @RequestParam(value = "test", required = false) boolean testing) throws Throwable {
-
-    LOGGER
-        .info("entering controller: userId={}, creditCardState={}, deviceState={}, testing={}", userId, creditCardState,
-              deviceState, testing);
-
-    if (creditCardState != null && !isCreditCardStateValid(creditCardState)) {
-      throw new IllegalArgumentException("creditCardState is not valid");
-    }
-
-    if (deviceState != null && !isDeviceStateValid(deviceState)) {
-      throw new IllegalArgumentException("deviceState is not valid");
-    }
-
-    String token = tokenService.getToken();
-
-    final CompletableFuture<UserResponse> userFuture = userService.getUser(token, userId);
-
-    final CompletableFuture<List<CreditCard>> creditCardsFuture = creditCardService
-        .getCreditCards(token, userId, creditCardState);
-
-    final CompletableFuture<List<Device>> devicesFuture = deviceService
-        .getDevices(token, userId, deviceState);
-
-    final CompletableFuture<Void> allOf = CompletableFuture
-        .allOf(userFuture, creditCardsFuture, devicesFuture);
-
-
-    User user = new User();
+      @RequestParam(value = "deviceState", required = false) String deviceState) {
 
     try {
+      LOGGER
+          .info("entering controller: userId={}, creditCardState={}, deviceState={}", userId,
+                creditCardState,
+                deviceState);
+
+      if (creditCardState != null && !isCreditCardStateValid(creditCardState)) {
+        throw new IllegalArgumentException("creditCardState is not valid");
+      }
+
+      if (deviceState != null && !isDeviceStateValid(deviceState)) {
+        throw new IllegalArgumentException("deviceState is not valid");
+      }
+
+      String token = tokenService.getToken();
+
+      final CompletableFuture<UserResponse> userFuture = userService.getUser(token, userId);
+
+      final CompletableFuture<List<CreditCard>> creditCardsFuture = creditCardService
+          .getCreditCards(token, userId, creditCardState);
+
+      final CompletableFuture<List<Device>> devicesFuture = deviceService
+          .getDevices(token, userId, deviceState);
+
+      final CompletableFuture<Void> allOf = CompletableFuture
+          .allOf(userFuture, creditCardsFuture, devicesFuture);
+
+      User user = new User();
+
       allOf.join();
 
       UserResponse userResponse = userFuture.get();
@@ -99,51 +97,41 @@ public class Controller {
       List<Device> devices = devicesFuture.get();
       user.setDevices(devices);
 
-      user.add(linkTo(methodOn(Controller.class).composite(userId, creditCardState, deviceState, testing)).withSelfRel());
+      user.add(
+          linkTo(methodOn(Controller.class).composite(userId, creditCardState, deviceState)).withSelfRel());
 
+      return user;
     }
-    catch (InterruptedException e) {
-      e.printStackTrace();
+    catch (IllegalArgumentException e) {
+      throw e;
     }
-    catch (ExecutionException e) {
-      throw e.getCause();
+    catch (Exception e) {
+      throw new InternalException();
     }
 
-    return user;
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  String handleIllegalArgumentException(IllegalArgumentException e) {
+    return e.getMessage();
+  }
+
+  @ExceptionHandler(InternalException.class)
+  String handleInternalException(Exception e) {
+    return "I'm sorry, but an internal error has happened. Please contact support.";
+  }
+
+  private boolean isCreditCardStateValid(final String creditCardState) {
+    return creditCardState != null && !creditCardState.isBlank() && creditCardStatePattern.matcher(creditCardState)
+                                                                                          .matches();
   }
 
   private boolean isDeviceStateValid(final String deviceState) {
     return deviceState != null && !deviceState.isBlank() && deviceStatePattern.matcher(deviceState).matches();
   }
 
-  private boolean isCreditCardStateValid(final String creditCardState) {
-    return creditCardState != null && !creditCardState.isBlank() && creditCardStatePattern.matcher(creditCardState).matches();
+  private class InternalException extends RuntimeException {
+
   }
-
-//  private List<CreditCard> createTestCreditCards() {
-//    return null;
-//  }
-//
-//  private List<Device> createTestDevices() {
-//    return null;
-//  }
-
-//  @ExceptionHandler(Exception.class)
-//  @GetMapping("/error")
-//  public String error(Exception e) {
-//    e.printStackTrace();
-//    return "Error: " + e.getMessage();
-//  }
-
-//  @GetMapping("/error")
-//  public String error() {
-//    return "Error";
-//  }
-
-//  private User createTestUser() {
-//    User user = new User();
-//    user.setId("132fc356-a6ab-4abe-867d-86c493a607cc");
-//    return user;
-//  }
 
 }
